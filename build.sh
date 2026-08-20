c06dae8b83136e140fd90a5d3ce7801ab498d4c5
root <root@ip-172-31-14-152.ap-northeast-3.compute.internal>
2026-08-20 09:18:40 +0000

    Initial CI/CD Docker project

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#login into DockerHub:
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+
+#stopping existing container:
+docker stop naughty_jennings
+docker rm naughty_jennings
+#building a image:
+docker build -t react-ci/cd .
+
+#running a container from the created image:
+docker run -d -it --name react -p 80:80 react-ci/cd
+
+#pushing the image to dockerhub:
+docker tag react-ci/cd vishwak24/cicd:ci-cd
+docker push vishwak24/cicdci-cd
